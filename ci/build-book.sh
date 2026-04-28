1323e259212f4f2f1fbe59d98daac719137987f6
James Muriuki <geniusinrust@gmail.com>
2026-04-28 20:24:36 +0300

    Update mdbook version to 0.4 in build script

--- ci/build-book.sh
+++ ci/build-book.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cargo install --vers ^0.3 mdbook --debug
+cargo install --vers ^0.4 mdbook --debug
 cargo install -f --path mdbook-skill-tree --debug
 cd book
 mdbook-skill-tree install
